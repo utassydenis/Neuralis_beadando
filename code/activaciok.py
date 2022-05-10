c0cb2fcb79136bc796e90fa6d9921a9342057268
Dénes <utassy.denis@gmail.com>
2022-05-10 10:59:08 +0200

    New Code

--- code/activaciok.py
+++
@@ -1,8 +0,0 @@
-from activation_reteg import Activacio
-import numpy as np
-
-class Tanh(Activacio):
-    def __init__(self):
-        tanh = lambda x: np.tanh(x)
-        dacti_tanh = lambda x: 1 - np.tanh(x) ** 2
-        super(Tanh, self).__init__(tanh,dacti_tanh)
--- code/main.py
+++ code/main.py
@@ -1,6 +1,20 @@
 import numpy as np
-from retegek import Retegek
-from activaciok import Tanh
+from sklearn.metrics import mean_squared_error
+
+def activation_tanh(x):
+    return np.tanh(x)
+
+
+def dactivation_tanh(x):
+    return 1.0 - x**2
+
+
+def activation_sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+def dactivation_sigmoid(x):
+    return x*(1.0-x)
 
 #Adatfeldolgozás
 
@@ -25,37 +39,110 @@ train_y = (np.array([train[i+5] for i in range(len(train)-5)])-5)/45050
 test_x = (np.array([test[i:i+5] for i in range(len(test)-5)])-5)/45050
 test_y = (np.array([test[i+5] for i in range(len(test)-5)])-5)/45050
 
-# Neurálisháló
-def mse(elvart_output, output):
-    return np.mean(np.power(elvart_output - output, 2))
-
-def dacti_mse(elvart_output, output):
-    return 2 * (output - elvart_output)/np.size(elvart_output)
 
 
-network = [
-    Retegek(len(train_x[0]), 10),
-    Tanh(),
-    Retegek(10, 7),
-    Tanh(),
-    Retegek(7, len(train_y[0])),
-    Tanh()
-]
+# Neurális háló
+network = [len(train_x[0]), 7, 8, 10, len(train_y[0])]
 
-epochs = 100
+sulyok = [np.linspace(-1, 1, network[i]*network[i+1]).reshape((network[i+1], network[i])) for i in range(len(network)-1)]
 
-learning_rate = 0.1
+# Létrehozzuk a súlyokat(és biast) a layerek neuronjai alapján
 
-for i in range(epochs):
-    error = 0
-    for x, y in zip(train_x, train_y):
-        output = x
-        for layer in network:
-            output = layer.forward(output)
-        error += mse(y, output)
-        grad = dacti_mse(y, output)
-        for layer in reversed(network):
-            grad = layer.backward(grad, learning_rate)
+biases = [np.linspace(-1, 1, (len(network)-1)*1)]
+biases = biases[0]
 
-    error /= len(train_x)
-    print("%d/%d, error=%f" % (i + 1, epochs, error))
+
+#Training
+epoch = 0
+learning_rate = 0.001
+sumerr = len(train_x)
+
+ml = [np.zeros((network[l+1])) for l in range(len(network)-1)]
+vl = [np.zeros((network[l+1])) for l in range(len(network)-1)]
+delta = [np.zeros((network[l+1])) for l in range(len(network)-1)]
+
+
+#while sumerr/len(train_x) >= 0.0000001 and epoch < 200:
+while epoch < 500:
+    sumerr = 0.0
+    epoch += 1
+    for input, expected_output in zip(train_x, train_y):
+        # forward propagation start
+        dactivated_input = []
+        activated_input = []
+        for suly, bias in zip(sulyok, biases):
+            input = (np.dot(input.T, suly.T)+bias).T  # mátrix szorzás + bias
+            input = activation_sigmoid(input)  # eredmény aktiválása
+
+            # backpropnak
+            dactivated_input.append(dactivation_sigmoid(input))   # eredmény deaktiválása és eltárolása a backpropnak
+            activated_input.append(input)
+
+            #backpropnak
+
+        predicted_output = input
+        # forward propagation end
+
+        # backprop start
+        error = expected_output - predicted_output  # hiba nagysága
+        delta_output = error * dactivated_input[-1]
+
+
+        for i in reversed(range(len(network)-1)):
+            if i == len(network) - 2:
+                delta[i][:] = error * dactivated_input[-1]
+            else:
+                np.dot(delta[i + 1], sulyok[i + 1], out=delta[i])
+                delta[i] *= dactivated_input[i+1][0]
+
+
+
+        for i in reversed(range(len(network)-1)):
+            gradiens = np.dot(activated_input[i], delta[i].reshape(len(delta[i]),1).T)
+            ml[i] = 0.9*ml[i]+(1-0.9)*np.array(gradiens)
+            vl[i] = 0.999 * vl[i] + (1-0.999)*np.power(gradiens,2)
+            mh = ml[i]/(1-np.power(0.9, epoch+1))
+            vh = vl[i]/(1-np.power(0.999, epoch+1))
+            divided = np.divide(np.dot(learning_rate, mh), (np.sqrt(vh) + 0.00000001))
+            sulyok[i] = np.add(np.array(sulyok[i]).T,divided[0])
+            sulyok[i] = sulyok[i].T
+
+
+    sumerr += sum([error[j] ** 2 for j in range(network[-1])])
+    print(epoch, sumerr)
+
+sumerr = 0.0
+y2 = []
+for input in test_x:
+    for suly, bias in zip(sulyok, biases):
+        input = (np.dot(input.T, suly.T) + bias).T  # mátrix szorzás + bias
+        input = activation_sigmoid(input)  # eredmény aktiválása
+    y2.append(activation_sigmoid(input[0]))
+
+for predicted , expected in zip(y2,test_y):
+    print(predicted)
+    print(expected)
+    print("")
+print(mean_squared_error(y2, test_y))
+"""
+        # biasok beállítása
+        for delta, bias in zip(reversed(delta), biases):
+            bias += np.sum(delta, axis=0)*learning_rate
+
+
+    sumerr += sum([error[j] ** 2 for j in range(network[-1])])
+    print(epoch, sumerr)
+
+
+
+sumerr = 0.0
+y2 = []
+for input in test_x:
+    for suly, bias in zip(sulyok, biases):
+        input = (np.dot(input.T, suly.T) + bias).T  # mátrix szorzás + bias
+        input = activation_sigmoid(input)  # eredmény aktiválása
+    y2.append(activation_sigmoid(input[0]))
+print(y2)
+print(test_y)
+print(mean_squared_error(y2,test_y))
+"""
